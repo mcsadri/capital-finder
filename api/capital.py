07095101e22f60cafe164a5c6f135e93f0ff79cf
Manuch Sadri <colonquebang@hotmail.com>
2023-05-09 21:57:43 -0700

    updated capital.py handler class to add boilerplate code for converting the url query params to a dictionary, and creating the message for output

--- api/capital.py
+++ api/capital.py
@@ -4,7 +4,29 @@ import requests
 
 
 class handler(BaseHTTPRequestHandler):
-    pass
+    def do_GET(self):
+        # create a dictionary of parameters from the url string
+        url = self.path
+        url_components = parse.urlsplit(url)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dictionary = dict(query_string_list)
+
+        # do the stuff
+
+
+
+
+        # form the response
+        # set the message content
+        message = "doodah"
+        # HTTP code
+        self.send_response(200)
+        # define the content type
+        self.send_header('Content-type', 'text/plain')
+        # add a blank line
+        self.end_headers()
+        # write the message
+        self.wfile.write(message.encode())
 
 
 if __name__ == '__main__':
