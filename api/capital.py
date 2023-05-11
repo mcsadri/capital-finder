d3dc93faf168ee6b16e826b14e243019ab88f0fb
Manuch Sadri <colonquebang@hotmail.com>
2023-05-10 22:12:55 -0700

    progress on adding API call and response handling

--- api/capital.py
+++ api/capital.py
@@ -10,29 +10,58 @@ class handler(BaseHTTPRequestHandler):
         url_components = parse.urlsplit(url)
         query_string_list = parse.parse_qsl(url_components.query)
         dictionary = dict(query_string_list)
+        # print("dictionary: ", dictionary)
 
         # localhost testing URLs
         # http://localhost:8000/api/capital?name=spain
 
         # do the stuff
-        print("capital-finder")
-        country_api_url = f"https://restcountries.com/v3.1/name/{dictionary.get('name')}"
-        print("the country name search api url is:", country_api_url)
-        country_response = requests.get(country_api_url)
-        country_data = country_response.json()
-        # print("the country response is:", country_data)
-        # country_capital = country_data[0]["capital"][0]
-        # print("the capital is:", country_capital)
+        country_capital = str()
+        capital_country = str()
+        if dictionary.get("name"):
+            print("capital-finder")
 
+            # given the name of a country get the name of the capital city
+            country_api_url = f"https://restcountries.com/v3.1/name/{dictionary.get('name')}"
+            # print("the country name search api url is:", country_api_url)
+            country_response = requests.get(country_api_url)
+            country_data = country_response.json()
 
-        # capital_api_url = f"https://restcountries.com/v3.1/capital/{dictionary.get('name')}"
+            # if the name doesn't match a country
+            if country_data["status"] == 404:
+                country_capital = None
+                print("that country name was not found")
 
+                # search if the name matches a country's capital city
+                capital_api_url = f"https://restcountries.com/v3.1/capital/{dictionary.get('name')}"
+                capital_response = requests.get(capital_api_url)
+                capital_data = capital_response.json()
+                print("capital_data is:", capital_data)
 
+                if capital_data["status"] == 404:
+                    capital_country = None
+                    print("that capital name was not found")
+                else:
+                    capital_country = capital_data[0]["name"]["common"]
+                    print("the country is:", capital_country)
+                    print(f"{dictionary.get('name')} is the capital of {capital_country}")
 
+            # if the query name matched a country name
+            else:
+                country_capital = country_data[0]["capital"][0]
+                print("the capital is:", country_capital)
 
         # form the response
         # set the message content
+        # if country_capital is None and capital_country is None:
+        #     message = f"The provided location, {dictionary.get('name').upper()}, was not found."
+        # elif country_capital:
+        #     message = f"The capital of {dictionary.get('name').upper()} is {country_capital}."
+        # else:
+        #     message = f"{dictionary.get('name').upper()} is the capital of {capital_country}"
+
         message = "doodah"
+
         # HTTP code
         self.send_response(200)
         # define the content type
